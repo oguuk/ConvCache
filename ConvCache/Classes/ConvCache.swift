b32a4de6ef27aef52b0846bcac0beacaf53b4c42
Gukwon Oh <ogw135@gmail.com>
2023-04-09 15:09:49 +0900

    [feat] : Fetch data from URL and handle caching and errors

--- ConvCache/Classes/ConvCache.swift
+++ ConvCache/Classes/ConvCache.swift
@@ -52,3 +52,45 @@ public class ConvCache {
         }
     }
     
+    private func get(URLStr: String, etag: String? = nil, completion: @escaping (Result<CacheableData, Error>) -> Void) {
+        guard let url = URL(string: URLStr) else { return completion(.failure(CacheErrorType.networkError)) }
+        var request = URLRequest(url: url)
+        
+        if let etag = etag {
+            request.addValue(etag, forHTTPHeaderField: "If-None-Match")
+        }
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(CacheErrorType.networkError))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(CacheErrorType.notFound))
+                return
+            }
+            
+            switch httpResponse.statusCode {
+            case 200..<300:
+                let etag = httpResponse.allHeaderFields["Etag"] as? String ?? ""
+                let data = CacheableData(cachedData: data, etag: etag)
+                self.saveIntoCache(url: url, data: data)
+                self.saveIntoDiskByLRU(url: url, data: data)
+                completion(.success(data))
+            case 304:
+                completion(.failure(CacheErrorType.notModified))
+            case 402:
+                completion(.failure(CacheErrorType.paymentRequired))
+            default:
+                completion(.failure(CacheErrorType.networkError))
+            }
+        }
+        
+        task.resume()
+    }
