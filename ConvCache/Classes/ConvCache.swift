a727b23ac9d66fcdfe255d55f675825be27a51aa
Gukwon Oh <ogw135@gmail.com>
2023-04-09 15:08:49 +0900

    [feat] : Cache Bytes Setting

--- ConvCache/Classes/ConvCache.swift
+++ ConvCache/Classes/ConvCache.swift
@@ -10,3 +10,8 @@ public class ConvCache {
         let size100MB = 104857600
         cache.configureCacheSize(with: size50MB, with: size100MB)
     }
+    
+    public func configureCache(with maximumMemoryBytes: Int, with maximumDiskBytes: Int) {
+        cache.configureCacheSize(with: maximumMemoryBytes, with: maximumDiskBytes)
+    }
+    
