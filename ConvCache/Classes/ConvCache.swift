8c703a408bcc325745d005a4fa6d80fa5bd8115c
Gukwon Oh <ogw135@gmail.com>
2023-04-09 15:09:31 +0900

    [feat] : Caching process

--- ConvCache/Classes/ConvCache.swift
+++ ConvCache/Classes/ConvCache.swift
@@ -15,3 +15,40 @@ public class ConvCache {
         cache.configureCacheSize(with: maximumMemoryBytes, with: maximumDiskBytes)
     }
     
+    public func setData(URLStr: String, completion: @escaping (Result<Data,Error>) -> Void) {
+        guard let url = URL(string: URLStr) else { return completion(.failure(CacheErrorType.networkError)) }
+        
+        // NSCache 확인
+        if let data = self.checkMemory(url: url) {
+            get(URLStr: "\(url)", etag: data.cacheInfo.etag) { (result) in
+                switch result  {
+                case let .success(cacheableData):
+                    completion(.success(cacheableData.cahedData))
+                case let .failure(error):
+                    completion(.failure(error))
+                }
+            }
+            return
+        }
+        
+        if let data = self.checkDisk(url: url) {
+            get(URLStr: "\(url)", etag: data.cacheInfo.etag) { (result) in
+                switch result {
+                case let .success(cacheableData):
+                    completion(.success(cacheableData.cahedData))
+                case let .failure(error):
+                    completion(.failure(error))
+                }
+            }
+        }
+        
+        get(URLStr: "\(url)") { result in
+            switch result {
+            case let .success(cacheableData):
+                completion(.success(cacheableData.cahedData))
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
